07b3ed285f08d22a733003024c56ac63a2f12b9f
Rohaid <rohaidb1999@gmail.com>
2022-09-30 00:20:12 -0400

    Added TMP + Grid + Location Detection

--- Assets/Scripts/DragNDrop.cs
+++ Assets/Scripts/DragNDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class DragNDrop : MonoBehaviour, IPointerDownHandler , IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    //TODO:: Add clamping to the items so they can only be within the area of the box or the grid. 
     private RectTransform trans;
     [SerializeField]
     private Canvas canvas;
@@ -13,23 +14,23 @@ public class DragNDrop : MonoBehaviour, IPointerDownHandler , IBeginDragHandler,
        trans = GetComponent<RectTransform>(); 
        group = GetComponent<CanvasGroup>();
     }
+    //Don't think this is necessary but too afraid to remove
     public void OnPointerDown(PointerEventData eventData){
-       Debug.Log("PointerDown");
     }
 
     public void OnDrag(PointerEventData eventData){
-        Debug.Log("Moving Around");
+        //getting change of position of the square / by the scale of the canvas
+        //Without it the square might not be at the right place.
         trans.anchoredPosition += (eventData.delta/canvas.scaleFactor);
         
     }
 
     public void OnBeginDrag(PointerEventData eventData){
         group.blocksRaycasts = false;
-        Debug.Log("Started Dragging");
     }
     
     public void OnEndDrag(PointerEventData eventData){
-        Debug.Log("Stopped Dragging");
+        //allows the unit script/ui to detect the item
          group.blocksRaycasts = true;
     }
     
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,13 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Unit : MonoBehaviour, IDropHandler
 {
+    //TODO:: Test Grid Calculation/See if using the grid layout component + getting the rec max X and mmin Y can solve some issues
+    [SerializeField] 
+    private RectTransform Map;
+    [SerializeField]
+    private TextMeshProUGUI DummyText;
+    private RectTransform currSquare;
+    float xPos;
+    float yPos;
+    void Start()
+    {
+        // Calculates which square it is in the grid.
+        currSquare = GetComponent<RectTransform>();
+
+             xPos = currSquare.anchoredPosition.x / currSquare.rect.width;
+             yPos = currSquare.anchoredPosition.y / currSquare.rect.height;
+            if (xPos > 0) {
+                xPos  = Mathf.CeilToInt(xPos);
+            } else {
+                xPos = Mathf.FloorToInt(xPos);
+            }
+            if (yPos > 0){
+                yPos = Mathf.CeilToInt(yPos);
+            } else {
+                yPos = Mathf.FloorToInt(yPos);
+            }
+            
+    }
     public void OnDrop(PointerEventData data){
+       
         if(data.pointerDrag != null){
+            //snaps item to grid
             data.pointerDrag.GetComponent<RectTransform>().anchoredPosition = 
-            GetComponent<RectTransform>().anchoredPosition;
+                currSquare.anchoredPosition + Map.anchoredPosition;
+
+                //Testing purposes only
+            DummyText.text = "This will not be shown to the user. \n At Square : " + xPos +  "," + yPos;
         }
     }
 }
