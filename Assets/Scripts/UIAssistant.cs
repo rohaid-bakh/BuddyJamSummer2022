c751da269c104dbe7736c1506e43ba9508f0ed6e
Alex <a.beteta9@gmail.com>
2022-09-30 20:29:12 -0700

    Added sprite swap on button press

--- Assets/Scripts/UIAssistant.cs
+++ Assets/Scripts/UIAssistant.cs
@@ -14,25 +14,34 @@ public class UIAssistant : MonoBehaviour
     [SerializeField] float textSpeed;
 
     [SerializeField] int messageArrayNumber = 0;
+
+    [SerializeField] Image npcImage;
+
+
+    Color[] colors = new Color[] { Color.black, Color.white, Color.yellow, Color.blue };
+    public Sprite[] npcSprites;
+
+
+    string[] messageArray = new string[]
+    {
+    "This city is great! The supermarket is in the middle of town. It's only two blocks from my house on the east side.",
+    "The playground I like is on the west side next to the courthouse.",
+    "I work at the courthouse on the south side of town. The playground to the west of my office is too loud!",
+    "I live at the southeast point of the city. I have to walk straight across town just to visit the playground!",
+    };
+
+
     private void Awake()
     {
         messageText = transform.Find("Message").Find("MessageText").GetComponent<TMP_Text>();
+        npcImage.color = Color.black;
     }
 
     public void NextButton()
     {
-        //Debug.Log("next");
-
-
-        string[] messageArray = new string[]
-        {
-            "This city is great! The supermarket is in the middle of town. It's only two blocks from my house on the east side.",
-            "The playground I like is on the west side next to the courthouse.",
-            "I work at the courthouse on the south side of town. The playground to the west of my office is too loud!",
-            "I live at the southeast point of the city. I have to walk straight across town just to visit the playground!",
-        };
 
         messageArrayNumber++;
+
 
         if (messageArrayNumber >= messageArray.Length)
         {
@@ -43,45 +52,44 @@ public class UIAssistant : MonoBehaviour
         string message = messageArray[messageArrayNumber];
         TextWriter.AddWriter_Static(messageText, message, textSpeed, true, true);
 
+        //change color of sprite
+        npcImage.color = colors[messageArrayNumber];
+        //change sprite
+        npcImage.sprite = npcSprites[messageArrayNumber];
+
         
     }
 
     public void PreviousButton()
     {
-        string[] messageArray = new string[]
-        {
-            "This city is great! The supermarket is in the middle of town. It's only two blocks from my house on the east side.",
-            "The playground I like is on the west side next to the courthouse.",
-            "I work at the courthouse on the south side of town. The playground to the west of my office is too loud!",
-            "I live at the southeast point of the city. I have to walk straight across town just to visit the playground!",
-        };
 
         messageArrayNumber--;
 
         if (messageArrayNumber < 0)
         {
-            messageArrayNumber = 2;
+            messageArrayNumber = 3;
         }
 
         string message = messageArray[messageArrayNumber];
         TextWriter.AddWriter_Static(messageText, message, textSpeed, true, true);
 
-        
+        //change color of sprite
+        npcImage.color = colors[messageArrayNumber];
+        //change sprite
+        npcImage.sprite = npcSprites[messageArrayNumber];
     }
 
     private void Start()
     {
         //TextWriter.AddWriter_Static(messageText, StepOne, textSpeed, true);
-        string[] messageArray = new string[]
-{
-            "This city is great! The supermarket is in the middle of town. It's only two blocks from my house on the east side.",
-            "The playground I like is on the west side next to the courthouse.",
-            "I work at the courthouse on the south side of town. The playground to the west of my office is too loud!",
-            "I live at the southeast point of the city. I have to walk straight across town just to visit the playground!",
-};
 
         string message = messageArray[0];
         TextWriter.AddWriter_Static(messageText, message, textSpeed, true, true);
+
+        //change color of sprite
+        npcImage.color = colors[messageArrayNumber];
+        //change sprite
+        npcImage.sprite = npcSprites[messageArrayNumber];
     }
 
 }
