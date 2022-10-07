3bd1fb6d4e9981e36cdc31c4648f4e4ca2d2287a
Rohaid <rohaidb1999@gmail.com>
2022-10-07 17:57:10 -0400

    Revert "Merge branch 'AlexAudioFix' into main"
    This reverts commit a45cc26cc33ff081a247515dc89ead8681beeb27.

--- Assets/Scripts/TutorialScreen.cs
+++ Assets/Scripts/TutorialScreen.cs
@@ -4,30 +4,5 @@ using UnityEngine;
 
 public class TutorialScreen : MonoBehaviour
 {
-    private void Awake()
-    {
-        Time.timeScale = 0f;
-        
-    }
 
-    public void OpenTutorial()
-    {
-        Time.timeScale = 0f;
-        tutorialPanel.SetActive(true);
-        FindObjectOfType<AudioManager>().Stop("TextSFX");
-
-    }
-
-    [SerializeField] GameObject tutorialPanel;
-    public void CloseTutorial()
-    {
-        Time.timeScale = 1f;
-        //FindObjectOfType<AudioManager>().Play("TextSFX");
-        if (!FindObjectOfType<PauseMenu>().textFinished)
-        {
-            FindObjectOfType<AudioManager>().Play("TextSFX");
-        }
-        tutorialPanel.SetActive(false);
-        
-    }
 }
