6df7fb23cb4452216a9b5368f1ef553ee925a3ea
Rohaid Bakheet <38147450+rohaid-bakh@users.noreply.github.com>
2022-10-07 17:52:16 -0400

    Revert "Merge pull request #11 from rohaid-bakh/RoEndCard"
    This reverts commit 0229fe6a681683b4a639e86defb72fc27f81bbbd.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,12 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public bool startTutorial;
-
     public void StartGame()
     {
-        //on start of game, say load tutorial to true, then set it to false in main scene
-        //startTutorial = true;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
